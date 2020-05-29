b3e0d807e1377d7f49c56ca44eb28402ad3de02c
nguyen nguyen <e1800938@edu.vamk.fi>
2020-05-29 14:50:17 +0300

    First set up with Formik and Yup

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  ActivityIndicator,
+  TextInput,
+  Button,
+  Text,
+  View,
+} from "react-native";
+import { Formik } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object().shape({
+  name: yup
+    .string()
+    .required()
+    .label("Name")
+    .min(2, "At least 2 characters")
+    .max(15, "At mosr 15 characters"),
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Formik
+        initialValues={{ name: "" }}
+        onSubmit={(values, actions) => {
+          alert(JSON.stringify(values));
+          setTimeout(() => actions.setSubmitting(false), 1000);
+        }}
+        validationSchema={validationSchema}
+      >
+        {(formikProps) => (
+          <React.Fragment>
+            <View style={styles.input}>
+              <TextInput
+                placeholder='Type here'
+                onChangeText={formikProps.handleChange("name")}
+              />
+            </View>
+            <Text style={styles.errorHandler}>{formikProps.errors.name}</Text>
+            {formikProps.isSubmitting ? (
+              <ActivityIndicator />
+            ) : (
+              <Button title='Submit' onPress={formikProps.handleSubmit} />
+            )}
+          </React.Fragment>
+        )}
+      </Formik>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 90,
   },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 3,
+  },
+  errorHandler: { color: "red" },
 });
